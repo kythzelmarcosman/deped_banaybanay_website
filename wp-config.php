8d58f1d1dc6e7138b8a9278aa02411e4020ca785
kythzelmarcosman <osmankyth@gmail.com>
2026-03-24 13:15:50 +0800

    wp-config changes

--- wp-config.php
+++ wp-config.php
@@ -19,23 +19,15 @@
  */
 
 // ** Database settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define( 'DB_NAME', 'depedweb' );
-
-/** Database username */
-define( 'DB_USER', 'root' );
-
-/** Database password */
-define( 'DB_PASSWORD', '' );
-
-/** Database hostname */
-define( 'DB_HOST', 'localhost' );
-
-/** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8mb4' );
-
-/** The database collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+// ** Database settings ** //
+define( 'DB_NAME',     getenv('MYSQLDATABASE') ?: 'wordpress' );
+define( 'DB_USER',     getenv('MYSQLUSER')     ?: 'root' );
+define( 'DB_PASSWORD', getenv('MYSQLPASSWORD') ?: '' );
+define( 'DB_HOST',     (getenv('MYSQLHOST') && getenv('MYSQLPORT'))
+                         ? getenv('MYSQLHOST') . ':' . getenv('MYSQLPORT')
+                         : 'localhost' );
+define( 'DB_CHARSET',  'utf8' );
+define( 'DB_COLLATE',  '' );
 
 /**#@+
  * Authentication unique keys and salts.
