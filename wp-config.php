1566c188418c622072b85e6a610cf08a31cd0574
kythzelmarcosman <osmankyth@gmail.com>
2026-03-24 10:59:38 +0800

    gsdgs

--- wp-config.php
+++ wp-config.php
@@ -20,11 +20,18 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME',     getenv('MYSQLDATABASE'));
-define('DB_USER',     getenv('MYSQLUSER'));
-define('DB_PASSWORD', getenv('MYSQLPASSWORD'));
-define('DB_HOST',     getenv('MYSQLHOST') . ':' . getenv('MYSQLPORT'));
-define('DB_CHARSET',  'utf8');
+define( 'DB_NAME', getenv('MYSQLDATABASE') ?: 'depedweb' );
+
+/** Database username */
+define( 'DB_USER', getenv('MYSQLUSER') ?: 'root' );
+
+/** Database password */
+define( 'DB_PASSWORD', getenv('MYSQLPASSWORD') ?: '' );
+
+/** Database hostname */
+$db_host = getenv('MYSQLHOST') ?: 'localhost';
+$db_port = getenv('MYSQLPORT') ?: '3306';
+define( 'DB_HOST', $db_host . ':' . $db_port );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
