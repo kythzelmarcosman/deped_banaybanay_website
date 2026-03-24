bc1a2715eb5b7c413883f563ac9e1a386fe1b773
kythzelmarcosman <osmankyth@gmail.com>
2026-03-24 09:55:45 +0800

    new wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,17 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'depedweb' );
+/** The name of the database for WordPress */
+define( 'DB_NAME', $_ENV['MYSQLDATABASE'] ?: 'depedweb' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', $_ENV['MYSQLUSER'] ?: 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', $_ENV['MYSQLPASSWORD'] ?: '' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', $_ENV['MYSQLHOST'] ?: 'localhost' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
